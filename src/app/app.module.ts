efa461e9fb864ed13fa70a946fe93352fb90084b
Cade Ritter <ritter.cade@gmail.com>
2017-08-18 18:45:55 -0500

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TransactionsComponent } from '../src/app/transactions/transactions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransactionsComponent
   ],
   imports: [
     BrowserModule
