b9cf9d4980015ae709add51a90889d5ef4591d8a
Daniel DeGrasse <daniel@degrasse.com>
2019-09-07 18:08:34 -0500

    Fixed typo

--- src/app/config.ts
+++ src/app/config.ts
@@ -6,7 +6,7 @@ if (environment.production) {
   CONFIG = {
     api_url: 'http://ricebikesapp.rice.edu/api',
     cas_auth_url: 'https://idp.rice.edu/idp/profile/cas/login',
-    service_url: 'http://ricebikesapp.rice.edu/auth
+    service_url: 'http://ricebikesapp.rice.edu/auth'
   }
 } else {
   CONFIG = {
