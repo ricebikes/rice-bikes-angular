3b3661241c25834f97b6169b64cdc9aea4a51fe7
Christine Zhao <czhao028@gmail.com>
2019-09-09 00:13:35 -0500

    admin financial nginit written

--- src/app/components/admin-financial/admin-financial.component.ts
+++ src/app/components/admin-financial/admin-financial.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TransactionService } from "../../services/transaction.service";
+import {Transaction} from "../../models/transaction";
 @Component({
   selector: 'app-admin-financial',
   templateUrl: './admin-financial.component.html',
@@ -11,7 +12,18 @@ export class AdminFinancialComponent implements OnInit {
     private transactionService: TransactionService)
   {}
 
+  transactions: Transaction[];
+
   ngOnInit() {
+    var props = { complete: true , is_paid: true}; //completed and paid - tracking financial
+    this.transactionService.getTransactions(props).then(transactions =>{
+      for(var index = 0; index <= Object.keys(transactions).length; index++){
+        var theDate = new Date(transactions[index].date_created);
+        if(Math.abs(new Date().getMonth() - theDate.getMonth()) <= 5){ //change this 5 later: last 5 months
+          this.transactions.push(transactions[index]);
+        }
+      }
+    });
   }
 
 }
