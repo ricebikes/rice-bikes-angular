35eb1b041861706da801224bb93abed057fe9e36
Daniel DeGrasse <daniel@degrasse.com>
2020-01-06 00:01:10 -0700

    Starting to write business logic for orders component

--- src/app/components/orders/orders.component.ts
+++ src/app/components/orders/orders.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {OrderService} from '../../services/order.service';
+import {Order} from '../../models/order';
 
 @Component({
   selector: 'app-orders',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orders.component.css']
 })
 export class OrdersComponent implements OnInit {
+  // set start to one month in past
+  start = new Date(Date.now() - 1000 * 60 * 60 * 24 * 7 * 4);
+  // set end to right now
+  end = new Date();
 
-  constructor() { }
+  orders = [];
+
+  constructor(private orderService: OrderService) { }
 
   ngOnInit() {
+  }
+
+  /**
+   * Gets orders between the ranges of start and end date
+   */
+  getOrders() {
+    this.orderService.getOrders(this.start.getTime(), this.end.getTime())
+      .then(orders => this.orders = orders)
+      .catch(err => console.log(err));
   }
 
 }
