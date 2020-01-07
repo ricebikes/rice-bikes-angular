77193103683cbe4a7ea268172ba19daa5f46ff3b
Daniel DeGrasse <daniel@degrasse.com>
2020-01-07 12:41:46 -0600

    Revert "Starting to write business logic for orders component"
    This reverts commit 35eb1b041861706da801224bb93abed057fe9e36.

--- src/app/components/orders/orders.component.ts
+++ src/app/components/orders/orders.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {OrderService} from '../../services/order.service';
-import {Order} from '../../models/order';
 
 @Component({
   selector: 'app-orders',
@@ -8,25 +6,10 @@ import {Order} from '../../models/order';
   styleUrls: ['./orders.component.css']
 })
 export class OrdersComponent implements OnInit {
-  // set start to one month in past
-  start = new Date(Date.now() - 1000 * 60 * 60 * 24 * 7 * 4);
-  // set end to right now
-  end = new Date();
 
-  orders = [];
-
-  constructor(private orderService: OrderService) { }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  /**
-   * Gets orders between the ranges of start and end date
-   */
-  getOrders() {
-    this.orderService.getOrders(this.start.getTime(), this.end.getTime())
-      .then(orders => this.orders = orders)
-      .catch(err => console.log(err));
   }
 
 }
