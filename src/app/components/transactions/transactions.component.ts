f68fbe6e259a6021539117d6477953e05fe5c715
joshschaff <joshuaischaffer@gmail.com>
2022-08-24 17:17:23 -0500

    retrospec tab filtering

--- src/app/components/transactions/transactions.component.ts
+++ src/app/components/transactions/transactions.component.ts
@@ -10,15 +10,104 @@ import { Transaction } from '../../models/transaction';
 })
 export class TransactionsComponent implements OnInit {
   transactions: Transaction[];
+  filteredTransactions: Transaction[];
   loading = true;
+
+  objectKeys = Object.keys
+  // objects mapping from values to booleans
+  modelFilters = {};
+  sizeFilters = {};
+  colorFilters = {};
+  statusFilters = {
+    ordered: true,
+    arrived: true,
+    building: true,
+    built: true,
+    'for sale': true
+  }
+
 
   constructor(public transactionService: TransactionService) { }
 
   currentTab = 'active';
 
   ngOnInit(): void {
-    this.setTab(this.currentTab, { complete: false, refurb: false });
+    this.setTab(this.currentTab, { 
+      complete: false, 
+      refurb: false ,
+      $or: [ //only show retrospecs w/ "building" status on active tab
+        {transaction_type : {$ne: 'retrospec'} },
+        {status: 'building'}
+      ]
+    });
   }
+
+
+  // decription = "${size} / ${color}""
+  parseRetrospec(bike) {
+    const [size, color] = bike.description.split(" / ")
+    return {
+      size: size.toLowerCase(),
+      color: color.toLowerCase(),
+      model: bike.model.toLowerCase()
+    }
+  }
+
+  /**
+   * Used for filtering on Retrospec tab
+   * Populates filters for all found models, sizes and colors 
+   * for currently available retrospecs
+   */
+  initializeFilters() {
+    if (this.currentTab !== "retrospec") {
+      this.modelFilters = {}
+      this.sizeFilters = {}
+      this.colorFilters = {}
+    } else {
+      this.transactions.forEach(tx => {
+        tx.bikes.forEach(bike => {
+          const { model, size, color } = this.parseRetrospec(bike)
+          this.modelFilters[model] = true
+          this.sizeFilters[size] = true
+          this.colorFilters[color] = true
+        })
+      })
+    }
+  }
+
+  /**
+   * Check to see if a transaction is filtered
+   * Only filters on retrospec tab
+   * @param transaction 
+   */
+  isTransactionDisplayed(transaction: Transaction) {
+    if (this.currentTab != 'retrospec') return true
+    return transaction.bikes.filter(bike => {
+      const { model, size, color } = this.parseRetrospec(bike)
+      return this.modelFilters[model] &&
+        this.sizeFilters[size] &&
+        this.colorFilters[color] &&
+        this.statusFilters[transaction.status]
+    }).length >0
+  }
+
+  /**
+   * Filter transactions on retrospec tab
+   */
+  filterTransactions() {
+    this.filteredTransactions = this.transactions.filter(this.isTransactionDisplayed.bind(this))
+  }
+
+  /**
+   * Triggered by filter checkbox click
+   * @param filtersObj one of modelFilters, colorFilters, sizeFilters
+   * @param filter a key in filtersObj
+   */
+  toggleFilter(filtersObj, filter) {
+    filtersObj[filter] = !filtersObj[filter]
+    this.filterTransactions()
+  }
+
 
   /**
    * Sets the current tab to the given string. Accepts optional props to request the backend with.
@@ -39,6 +128,8 @@ export class TransactionsComponent implements OnInit {
     this.transactionService.getTransactions(props)
       .then(transactions => {
         this.transactions = transactions;
+        this.initializeFilters()
+        this.filterTransactions()
         this.loading = false;
       });
   }
