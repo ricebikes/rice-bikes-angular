6297b621ab5d371eff9d2512a55c217415f3a2ac
Jingwen Hu <jinghuw@gmail.com>
2023-12-26 17:51:29 -0800

    only ask for phone # if inpatient or retrospec build

--- src/app/components/new-customer-transaction/new-customer-transaction.component.ts
+++ src/app/components/new-customer-transaction/new-customer-transaction.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
-import {TransactionService} from '../../services/transaction.service';
-import {Customer} from '../../models/customer';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TransactionService } from '../../services/transaction.service';
+import { Customer } from '../../models/customer';
 
 @Component({
   selector: 'app-new-customer-transaction',
@@ -16,7 +16,11 @@ export class NewCustomerTransactionComponent implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     private transactionService: TransactionService,
-  ) {}
+  ) {
+    route.queryParams.subscribe(p => {
+      this.type = p.t;
+    });
+  }
 
   ngOnInit() {
     this.type = this.route.snapshot.queryParamMap.get('t')
@@ -24,6 +28,6 @@ export class NewCustomerTransactionComponent implements OnInit {
 
   submitTransaction(customer: Customer): void {
     this.transactionService.createTransaction(this.type, customer)
-        .then(trans => this.router.navigate(['/transactions', trans._id]));
+      .then(trans => this.router.navigate(['/transactions', trans._id]));
   }
 }
